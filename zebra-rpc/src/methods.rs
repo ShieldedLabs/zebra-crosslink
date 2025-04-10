f4d414d8a2c13aca65496ddbadb7edf51cf1c6b8
Alfredo Garcia <oxarbitrage@gmail.com>
2025-04-10 19:59:43 -0300

    feat(rpc): Add some missing fields to transaction object (#9329)
    * extend `getrawtransaction` verbose output
    
    * replace strings where possible, fix tests
    
    * fix orchard
    
    * add todos for script assembly code
    
    * fix an import
    
    * fmt
    
    * fmt
    
    * suggestion for #9329: change coinbase field and add valueSat field to match zcashd (#9407)
    
    * change coinbase field and add valueSat field to match zcashd
    
    * remove valueSat; update vectors
    
    ---------
    
    Co-authored-by: Conrado Gouvea <conrado@zfnd.org>

--- zebra-rpc/src/methods.rs
+++ zebra-rpc/src/methods.rs
@@ -7,7 +7,7 @@
 //! So this implementation follows the `zcashd` server and `lightwalletd` client implementations.
 
 use std::collections::HashMap;
-use std::{collections::HashSet, fmt::Debug, sync::Arc};
+use std::{collections::HashSet, fmt::Debug};
 
 use chrono::Utc;
 use futures::{stream::FuturesOrdered, StreamExt, TryFutureExt};
@@ -67,6 +67,7 @@ pub mod types;
 
 use types::GetRawMempool;
 use types::MempoolObject;
+use types::TransactionObject;
 
 pub mod get_block_template_rpcs;
 
@@ -1222,6 +1223,7 @@ where
                                         .try_into()
                                         .expect("should be less than max block height, i32::MAX"),
                                 ),
+                                &network,
                             ))
                         })
                         .collect();
@@ -1910,6 +1912,7 @@ where
                             tx.transaction.clone(),
                             None,
                             None,
+                            &self.network,
                         ))
                     } else {
                         let hex = tx.transaction.clone().into();
@@ -1933,6 +1936,7 @@ where
                     tx.tx.clone(),
                     Some(tx.height),
                     Some(tx.confirmations),
+                    &self.network,
                 ))
             } else {
                 let hex = tx.tx.into();
@@ -3050,7 +3054,7 @@ impl Default for GetBlockHash {
 /// Response to a `getrawtransaction` RPC request.
 ///
 /// See the notes for the [`Rpc::get_raw_transaction` method].
-#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize)]
+#[derive(Clone, Debug, PartialEq, serde::Serialize)]
 #[serde(untagged)]
 pub enum GetRawTransaction {
     /// The raw transaction, encoded as hex bytes.
@@ -3062,52 +3066,6 @@ pub enum GetRawTransaction {
 impl Default for GetRawTransaction {
     fn default() -> Self {
         Self::Object(TransactionObject::default())
-    }
-}
-
-/// A Transaction object as returned by `getrawtransaction` and `getblock` RPC
-/// requests.
-#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize)]
-pub struct TransactionObject {
-    /// The raw transaction, encoded as hex bytes.
-    #[serde(with = "hex")]
-    pub hex: SerializedTransaction,
-    /// The height of the block in the best chain that contains the tx or `None` if the tx is in
-    /// the mempool.
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub height: Option<u32>,
-    /// The height diff between the block containing the tx and the best chain tip + 1 or `None`
-    /// if the tx is in the mempool.
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub confirmations: Option<u32>,
-    // TODO: many fields not yet supported
-}
-
-impl Default for TransactionObject {
-    fn default() -> Self {
-        Self {
-            hex: SerializedTransaction::from(
-                [0u8; zebra_chain::transaction::MIN_TRANSPARENT_TX_SIZE as usize].to_vec(),
-            ),
-            height: Option::default(),
-            confirmations: Option::default(),
-        }
-    }
-}
-
-impl TransactionObject {
-    /// Converts `tx` and `height` into a new `GetRawTransaction` in the `verbose` format.
-    #[allow(clippy::unwrap_in_result)]
-    fn from_transaction(
-        tx: Arc<Transaction>,
-        height: Option<block::Height>,
-        confirmations: Option<u32>,
-    ) -> Self {
-        Self {
-            hex: tx.into(),
-            height: height.map(|height| height.0),
-            confirmations,
-        }
     }
 }
 
