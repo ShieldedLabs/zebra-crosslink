c98ab283c699535d6a49273500b6c3337ee673ee
Kris Nuttycombe <kris@nutty.land>
2025-04-18 14:57:45 -0600

    zebra-state: Return only the history tree root in GetBlockTemplateChainInfo response. (#9444)
    The `HistoryTree` type is intended for internal use within the node; it
    should not be exposed via the ReadStateService response. Instead, this
    response should simply include the hash needed for block template
    construction.

--- zebra-rpc/src/methods/tests/vectors.rs
+++ zebra-rpc/src/methods/tests/vectors.rs
@@ -9,6 +9,7 @@ use zebra_chain::{
     amount::Amount,
     block::Block,
     chain_tip::{mock::MockChainTip, NoChainTip},
+    history_tree::HistoryTree,
     parameters::Network::*,
     serialization::{ZcashDeserializeInto, ZcashSerialize},
     transaction::UnminedTxId,
@@ -54,7 +55,7 @@ async fn rpc_getinfo() {
         GetBlockTemplateChainInfo {
             tip_hash: Mainnet.genesis_hash(),
             tip_height: Height::MIN,
-            history_tree: Default::default(),
+            chain_history_root: HistoryTree::default().hash(),
             expected_difficulty: Default::default(),
             cur_time: zebra_chain::serialization::DateTime32::now(),
             min_time: zebra_chain::serialization::DateTime32::now(),
@@ -1902,7 +1903,7 @@ async fn rpc_getblocktemplate_mining_address(use_p2pkh: bool) {
                     cur_time: fake_cur_time,
                     min_time: fake_min_time,
                     max_time: fake_max_time,
-                    history_tree: fake_history_tree(&Mainnet),
+                    chain_history_root: fake_history_tree(&Mainnet).hash(),
                 }));
         }
     };
@@ -2375,7 +2376,7 @@ async fn rpc_getdifficulty() {
                 cur_time: fake_cur_time,
                 min_time: fake_min_time,
                 max_time: fake_max_time,
-                history_tree: fake_history_tree(&Mainnet),
+                chain_history_root: fake_history_tree(&Mainnet).hash(),
             }));
     };
 
@@ -2401,7 +2402,7 @@ async fn rpc_getdifficulty() {
                 cur_time: fake_cur_time,
                 min_time: fake_min_time,
                 max_time: fake_max_time,
-                history_tree: fake_history_tree(&Mainnet),
+                chain_history_root: fake_history_tree(&Mainnet).hash(),
             }));
     };
 
@@ -2424,7 +2425,7 @@ async fn rpc_getdifficulty() {
                 cur_time: fake_cur_time,
                 min_time: fake_min_time,
                 max_time: fake_max_time,
-                history_tree: fake_history_tree(&Mainnet),
+                chain_history_root: fake_history_tree(&Mainnet).hash(),
             }));
     };
 
@@ -2447,7 +2448,7 @@ async fn rpc_getdifficulty() {
                 cur_time: fake_cur_time,
                 min_time: fake_min_time,
                 max_time: fake_max_time,
-                history_tree: fake_history_tree(&Mainnet),
+                chain_history_root: fake_history_tree(&Mainnet).hash(),
             }));
     };
 
